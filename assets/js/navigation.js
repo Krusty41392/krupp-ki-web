0875ffabb1c8f2f3f5040473a4ec80a60005e43f
rene <r.krupp151276@outlook.de>
2024-02-20 17:02:25 +0100

    changed footer

--- assets/js/navigation.js
+++
@@ -1,8 +0,0 @@
-// JavaScript, um den Toggle-Button bei 1024px zu aktivieren
-window.addEventListener('DOMContentLoaded', function() {
-    if (window.matchMedia('(max-width: 1024px)').matches) {
-        // Menü anzeigen
-        var menu = document.getElementById('menu-toggle');
-        menu.style.display = 'block';
-    }
-});
--- footer.php
+++ footer.php
@@ -1,32 +1,35 @@
-<?php
-/**
- * Footer Section for our theme.
- *
- * @package ThemeGrill
- * @subpackage Ample
- * @since Ample 0.1
- */
-?>
-      </div><!-- .main-wrapper -->
+
+      </div>
 
       <footer id="colophon">
-         <div class="inner-wrap">
-            <?php get_sidebar( 'footer' ); ?>
+         <div class="container">
+            <div class="col-12">
+               test
+            </div>
+         </div>
+         <div class="container">
+            <div class="inner-wrap">
+               <?php get_sidebar( 'footer' ); ?>
 
-            <div class="footer-bottom clearfix">
-               <div class="copyright-info">
-                  <?php do_action( 'ample_footer_copyright' ); ?>
-               </div>
-
-               <div class="footer-nav">
-               <?php
-                  if ( has_nav_menu( 'footer' ) ) {
-                     wp_nav_menu( array( 'theme_location' => 'footer', 'depth' => -1 ) );
-                  }
-               ?>
+               <div class="footer-bottom clearfix">
+                  <div class="row">
+                     <div class="col-6">
+                        <p>© 2024 Krupp KI & Web. Alle Rechte vorbehalten.</p>
+                     </div>
+                     <div class="col-6">
+                        <div class="footer-nav">
+                           <?php
+                              if ( has_nav_menu( 'footer' ) ) {
+                                 wp_nav_menu( array( 'theme_location' => 'footer', 'depth' => -1 ) );
+                              }
+                           ?>
+                     </div>
+                  </div>
                </div>
             </div>
          </div>
+
+         
       </footer>
       <a href="#masthead" id="scroll-up"><i class="fa fa-angle-up"></i></a>
    </div><!-- #page -->
